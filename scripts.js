a37d126f18fb2b0bb3903372827bc065c2d6fb2e
Reza Lael <rezalaal@gmail.com>
2023-05-18 19:59:35 +0330

    Change some styles such as input[type=text], box-shadow, added java script link, event listener, open and close modal

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
-var btn = document.getElementById("add");
 
-console.log(btn);
+document.getElementById('add').addEventListener('click', function() {
+    document.getElementsByClassName('modal')[0].style.display = 'block';
+});
+
+function closeModal() {
+    document.getElementsByClassName('modal')[0].style.display = 'none';
+}
+
+document.querySelector(".closeModal").addEventListener('click', closeModal);
+
+document.querySelector(".close").addEventListener('click', closeModal);
+
+
+// DRY = Don't repeat yourself
