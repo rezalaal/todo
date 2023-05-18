a49362d84214d46782f2c2a673761d327e7164ef
Reza Lael <rezalaal@gmail.com>
2023-05-18 20:15:08 +0330

    lesson 4

--- scripts.js
+++ scripts.js
@@ -1,15 +1,3 @@
+var btn = document.getElementById("add");
 
-document.getElementById('add').addEventListener('click', function() {
-    document.getElementsByClassName('modal')[0].style.display = 'block';
-});
-
-function closeModal() {
-    document.getElementsByClassName('modal')[0].style.display = 'none';
-}
-
-document.querySelector(".closeModal").addEventListener('click', closeModal);
-
-document.querySelector(".close").addEventListener('click', closeModal);
-
-
-// DRY = Don't repeat yourself
+console.log(btn);
