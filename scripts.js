e99fb38b3d35997ec2c03a66746b5cb4fbba8521
Reza Lael <rezalaal@gmail.com>
2023-05-24 19:53:02 +0330

    saveTasks on localStorage & get them on page load

--- scripts.js
+++ scripts.js
@@ -13,3 +13,53 @@ document.querySelector(".close").addEventListener('click', closeModal);
 
 
 // DRY = Don't repeat yourself
+
+saveTaskBtn = document.querySelector("#saveTask");
+
+saveTaskBtn.addEventListener('click', saveTask);
+function saveTask() {
+
+    let task = document.querySelector("#task").value;
+    //validation 
+
+    if(task.length != 0) {
+        let allTasks = JSON.parse(localStorage.getItem('tasks'))
+        if(!allTasks) {
+            allTasks = []
+        }
+        
+        allTasks.push(task)
+    
+        localStorage.setItem('tasks', JSON.stringify(allTasks))
+
+        closeModal()
+    }else{
+        sp = document.querySelector(".footer");
+        sp.textContent = 'error';
+        sp.classList.add('error');
+
+    }
+
+   
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    let allTasks = JSON.parse(localStorage.getItem('tasks'))
+    if(!allTasks) {
+        allTasks = []
+    }
+
+    allTasks.forEach(function (item) {
+        document.querySelector("#taskItems").insertAdjacentHTML('beforeend', `
+        <div class="item">
+                <div class="item-circle"><i class="fa-sharp fa-solid fa-circle"></i></div>
+                <div class="item-title">${item}</div>
+                <div class="item-icon-ok"><i class="fa-solid fa-check"></i></div>
+                <div class="item-icon-edit"><i class="fa-solid fa-pen-to-square"></i></div>
+                <div class="item-icon-delete"><i class="fa-solid fa-trash"></i></div>
+            </div>
+        `)
+    });
+    
+})
