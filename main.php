41f754d4d3626ca09a785ddef33102a5ab668b28
Reza Lael <rezalaal@gmail.com>
2023-06-15 14:00:10 +0330

    constants

--- main.php
+++ main.php
@@ -2,6 +2,17 @@
     <head></head>
 
     <body>
-        <?= "Hello PHP"; ?>
+        <?php
+            echo PHP_VERSION;
+            echo "<br>";
+            echo PHP_OS;
+            echo "<br>";
+            echo PHP_DATADIR;
+            echo "<br>";
+            echo PHP_URL_PATH;
+            echo "<br>";
+            echo PHP_URL_PORT;
+            
+        ?>
     </body>
 </html>
