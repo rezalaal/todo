28a565e2665f273ffb9c7daf4f66be6d60d65572
Reza Lael <rezalaal@gmail.com>
2023-07-03 12:52:54 +0330

    example of fetch post json or form

--- api/index.php
+++ api/index.php
@@ -1,10 +1,13 @@
 <?php
 
+$data = json_decode(file_get_contents('php://input'), true);
 // add the Access-Control-Allow-Origin header to the server's response.
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+http_response_code(200);
+header('Content-Type: application/json');
 
 echo json_encode([
     'status' => true,
-    'message' => 'This is a test',
+    'message' => rand($data['min'], $data['max'])
 ]);
--- scripts.js
+++ scripts.js
@@ -1,23 +1,44 @@
-async function getQuote() {
-    await fetch('http://api.quotable.io/random')
-        .then(response => response.json())
-        .then(data => showOnContent(data))
-        .catch(error => console.error(error));
+// async function getRandomNumberFromAPI() {
+//     const formData = new FormData();
+//     formData.append('min', 0);
+//     formData.append('max', 999);
+  
+//     const response = await fetch('http://localhost:8009/', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/x-www-form-urlencoded'
+//       },
+//       body: new URLSearchParams(formData)
+//     });
+  
+//     const data = await response.json();
+//     console.log(data);
+//     addToMessageBox(data.message);
+//   }
+
+async function getRandomNumberFromAPI()
+{
+    const items = { min: 0, max: 999999 };
+
+   const response = await fetch('http://localhost:8009/', {
+    method: "POST",
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(items)
+   });   
+
+   const data = await response.json();
+   console.log(data)
+
+   addToMessageBox(data.message)
 }
 
-function showOnContent(data) {
-    document.querySelector('#content').innerHTML = `<p style='color:red;'>` + data.content + '</p>';
-    document.querySelector('#author'),innerHTML = data.author;
+function addToMessageBox(message) 
+{
+    const content = document.querySelector("#content");
+    content.innerHTML = 'Random number is: ' + message
+
 }
 
-function showTest(data) {
-    document.querySelector('#local').innerHTML = data.message;
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    fetch('http://localhost:8009')
-        .then(response => response.json())
-        .then(data => showTest(data))
-        .catch(error => console.error(error));
-    getQuote()
-});
+getRandomNumberFromAPI()
